1ef96f4a7231237bbc1608cc42a228f18998ae89
BarDeyvid <bardeyvid@gmail.com>
2025-10-17 11:51:49 -0300

    Corrigido caminho do vite para GitHub Pages

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const repoName = '/[WEGMine]'; 
+const repoName = '/WEGMine'; 
 
 export default defineConfig({
   plugins: [react()],
