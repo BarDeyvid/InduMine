1275c883309e5ea93b198c0498a2fc806070e84b
Deyvid Barcelos <bardeyvid@gmail.com>
2025-11-27 08:08:21 -0300

    fix: Got ThemeProvider to render again

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,9 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-const repoName = '/WEGMine'; 
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: repoName, 
-});
+  resolve: {
+    alias: {
+      // This forces all libraries to use the ONE copy of React in your root node_modules
+      react: path.resolve(__dirname, 'node_modules/react'),
+      'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
+      'styled-components': path.resolve(__dirname, 'node_modules/styled-components'),
+    },
+  },
+  optimizeDeps: {
+    // Forces Vite to pre-bundle these dependencies to avoid CommonJS/ESM mismatches
+    include: ['react', 'react-dom', 'styled-components', '@mui/material', '@emotion/react', '@emotion/styled'],
+  },
+})
