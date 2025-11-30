81254228c53fbba9e64eebfc4bd3d7255d5c7335
BarDeyvid <bardeyvid@gmail.com>
2025-11-30 11:44:53 -0300

    fix: Deploy/Dev modes

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -2,25 +2,29 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   resolve: {
     alias: {
-      // This forces all libraries to use the ONE copy of React in your root node_modules
       react: path.resolve(__dirname, 'node_modules/react'),
       'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
       'styled-components': path.resolve(__dirname, 'node_modules/styled-components'),
     },
   },
   optimizeDeps: {
-    // Forces Vite to pre-bundle these dependencies to avoid CommonJS/ESM mismatches
-    include: ['react', 'react-dom', 'styled-components', '@mui/material', '@emotion/react', '@emotion/styled'],
+    include: [
+      'react',
+      'react-dom',
+      'styled-components',
+      '@mui/material',
+      '@emotion/react',
+      '@emotion/styled',
+    ],
   },
   server: {
     watch: {
-      usePolling: true
+      usePolling: true,
     },
-    base: mode === 'deploy' ? '/WEGMine/' : '/',
-  }
-})
+  },
+  base: mode === 'deploy' ? '/WEGMine/' : '/',
+}))
