458b5bd57d4381e20fba785bd11c4a70c12b67c5
BarDeyvid <bardeyvid@gmail.com>
2025-12-01 01:31:38 -0300

    feat: Start of the Df Subdivision for better front-end over-all performance

--- data_to_api.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from urllib.parse import unquote
-
-df = pd.read_csv(r"data\grouped_products_final.csv")
-
-df["first_search"] = df["Product URL"].str.split("en/", n=1).str[1]
-df["second_search"] = df["first_search"].str.split("/", n=0).str[0]
-df["final_search"] = df["second_search"].apply(lambda x: unquote(x) if isinstance(x, str) else x)
-print(df["final_search"])
