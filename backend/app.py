768e5cc80f5491b54240c8cb4d262dd960a1e326
luclc13241 <lucasgabrielnardes@gmail.com>
2026-01-17 17:16:16 -0300

    start of refacturing

--- backend/app.py
+++ backend/app.py
@@ -22,7 +22,7 @@ from jose import JWTError, jwt
 
 # Database Config
 DB_USER = os.getenv("DB_USER", "root")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "Echidna12")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "Mv1208811#")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_NAME = os.getenv("DB_NAME", "indumine_db")
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}?charset=utf8mb4"
--- backend/config.py
+++ backend/config.py
@@ -11,7 +11,7 @@ class Settings(BaseSettings):
     mysql_host: str = os.getenv("MYSQL_HOST", "localhost")
     mysql_port: int = int(os.getenv("MYSQL_PORT", 3306))
     mysql_user: str = os.getenv("MYSQL_USER", "root")
-    mysql_password: str = os.getenv("MYSQL_PASSWORD", "")
+    mysql_password: str = os.getenv("MYSQL_PASSWORD", "Mv1208811#")
     mysql_database: str = os.getenv("MYSQL_DATABASE", "indumine_db")
     
     # MySQL Connection URL
